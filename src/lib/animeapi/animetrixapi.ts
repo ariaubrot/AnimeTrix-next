dabf31ae9b18164f02d443f65d8689f1263bbad8
ariaubrot <ariaputrapratamaaa@gmail.com>
2025-02-23 00:51:25 +0700

    Update animetrixapi.ts

--- src/lib/animeapi/animetrixapi.ts
+++ src/lib/animeapi/animetrixapi.ts
@@ -1,3 +1,3 @@
 export const AnimeApi = `https://api-consumet-org-psi-pearl.vercel.app/meta/anilist/meta/anilist`;
 export const AnifyApi = `https://anify.eltik.cc`;
-export const StreamApi = `${process.env.NEXT_PUBLIC_STREAM_API}/api/v2`;
+export const StreamApi = `https://api.amvstr.me/api/v2/stream`;
